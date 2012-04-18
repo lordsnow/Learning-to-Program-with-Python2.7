a761e4f2b61f9baaefe8cca33656fa822d470ed7
lordsnow <lordsnow@lordsnow.com>
2012-04-17 21:12:32 -0600

    exercise 2 and 3 complete.

---
+++ ex3.py
@@ -0,0 +1,25 @@
+# This line simply prints the string.
+print "I will now count my chickens:"
+# This line prints the string and calculates the number of hens.
+print "Hens", 25 + 30 /6
+# This line prints the string and calculates the number of roosters.
+print "Roosters", 100 - 25 * 3 % 4
+# This line simply prints the string.
+print "Now I will count the eggs:"
+# This line performs a calculation.
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# This line also prints a string.
+print "Is it true that 3 + 2 < 5 - 7?"
+# This line does a calculation and evaluates a boolean expression.
+print 3 + 2 < 5 - 7
+# The next two lines both print a string, and perform a calculation.
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+# The next line prints a string.
+print "Oh, that's why it's false."
+# The next line prints a string.
+print "How about some more."
+# The next three lines all print a string, and then perform a boolean expression.
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5<= -2
